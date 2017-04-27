c8ece30a78b1a623428f20475bc30402b6922acf
Nidhi Patel <nidhi@creolestudios.com>
2017-04-27 10:02:10 +0530

    Fix shadow in ios

--- libs/CardView.ios.js
+++ libs/CardView.ios.js
@@ -1,13 +1,14 @@
-import React, { Componet } from 'react';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-export default class CardView extends Componet {
+export default class CardView extends Component {
   render() {
     const { cardElevation, cornerRadius } = this.props;
     const ambientAlpha = (cardElevation + 10 + (cardElevation / 9.38)) / 100;
-    const color = rgba(0, 0, 0, ambientAlpha);
+    // const color = "rgba(0, 0, 0, "+ambientAlpha+")";
     return(
       <View style={{
-        shadowColor: color,
+        // shadowColor: color,
         shadowOffset: {
           width: 0,
           height: cardElevation
@@ -15,7 +16,7 @@ export default class CardView extends Componet {
         shadowRadius: cardElevation,
         shadowOpacity: 0.24
       }}>
-        {this.props.childern}
+        {this.props.children}
       </View>
     );
   }
