e642b89316dfc03884b98f450cd472809c9e9af4
Kishanjvaghela <kishan.j.vaghela@gmail.com>
2018-09-24 18:05:14 +0800

    fixed padding problem for pre lollipop

--- libs/CardView.android.js
+++ libs/CardView.android.js
@@ -1,35 +1,53 @@
 import { PropTypes } from 'prop-types';
-import { requireNativeComponent, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  requireNativeComponent,
+  View,
+  PixelRatio,
+  Platform
+} from 'react-native';
 var iface = {
   name: 'CardView',
   propTypes: {
-     cornerRadius: PropTypes.number,
-     cardElevation: PropTypes.number,
-cardMaxElevation: PropTypes.number,
-    ...View.propTypes, // include the default view properties
-  },
+    cornerRadius: PropTypes.number,
+    cardElevation: PropTypes.number,
+    cardMaxElevation: PropTypes.number,
+    ...View.propTypes // include the default view properties
+  }
 };
-module.exports = requireNativeComponent('RNCardView', iface);
 
-// const RNCardView = requireNativeComponent('RNCardView', CardView, {});
-// class CardView extends Component {
-//   render() {
-//     return (
-//       <RNCardView
-//         {...this.props}
-//       >
-// 	{this.props.children}
-//       </RNCardView>
-//     );
-//   }
-// }
-//
-// CardView.propTypes = {
-//   ...View.propTypes,
-//   cornerRadius: PropTypes.number,
-//   cardElevation: PropTypes.number,
-//   cardMaxElevation: PropTypes.number,
-// };
-//
-//
-// export default CardView;
+const RNCardView = requireNativeComponent('RNCardView', iface);
+class CardView extends Component {
+  render() {
+    if (Platform.Version < 21) {
+      const maxCardElevation = PixelRatio.getPixelSizeForLayoutSize(
+        this.props.cardMaxElevation
+      );
+      const cornerRadius = PixelRatio.getPixelSizeForLayoutSize(
+        this.props.cornerRadius
+      );
+      const cos45 = 0.52532198881;
+      const value = maxCardElevation * 1.5 + (1 - cos45) * cornerRadius;
+      return (
+        <RNCardView {...this.props}>
+          <View style={{ paddingBottom: value }}>{this.props.children}</View>
+        </RNCardView>
+      );
+    } else {
+      return <RNCardView {...this.props}>{this.props.children}</RNCardView>;
+    }
+  }
+}
+RNCardView.defaultProps = {
+  cornerRadius: 0,
+  cardElevation: 0,
+  cardMaxElevation: 0
+};
+
+RNCardView.propTypes = {
+  cornerRadius: PropTypes.number,
+  cardElevation: PropTypes.number,
+  cardMaxElevation: PropTypes.number
+};
+
+export default CardView;
