5243ae526656121c0849d2c2d15730323b3e8583
Hasta Ragil Saputra <hasta.ragil@gmail.com>
2019-07-29 14:13:08 +0700

    feat: support androidx

--- android/src/main/java/com/kishanjvaghela/cardview/RNCardView.java
+++ android/src/main/java/com/kishanjvaghela/cardview/RNCardView.java
@@ -1,7 +1,7 @@
 package com.kishanjvaghela.cardview;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
+import androidx.cardview.widget.CardView;
 import android.util.AttributeSet;
 
 import com.facebook.react.uimanager.PixelUtil;
